ba7ef0e70cb4e53d083e357da4b4a5fc65dc7916
sam629205 <285360060@qq.com>
2015-03-10 23:04:56 +0800

    1104

---
+++ index1.php
@@ -0,0 +1,27 @@
+<?php
+
+	 echo('1234');
+	$cookie_jar = '/tmp/cookie.tmp';
+	$ch = curl_init();//www.javzoo.com/ja/movie/4pmt
+	$options = array(CURLOPT_URL => 'http://www.javzoo.com/ja/movie/4pmt',
+			CURLOPT_HEADER => 0,
+			CURLOPT_NOBODY => 0,
+			CURLOPT_PORT => 80,
+			CURLOPT_POST => 0,
+			CURLOPT_RETURNTRANSFER => 1,
+			CURLOPT_FOLLOWLOCATION => 1,
+			CURLOPT_USERAGENT => 'Baiduspider+(+http://www.baidu.com/search/spider.htm)',
+			CURLOPT_ENCODING=>'gzip,deflate',//GZIP解压
+//			CURLOPT_COOKIEJAR => $cookie_jar,
+//			CURLOPT_COOKIEFILE => $cookie_jar,
+			CURLOPT_REFERER => 'http://www.baidu.com/search/spider.html',
+			CURLOPT_CONNECTTIMEOUT=> 40
+	);
+	curl_setopt_array($ch, $options);
+	$code = curl_exec($ch);
+	curl_close($ch);
+	echo $code;
+	
+	
+	
+?>
